f21219facf4fa4090a65cdccb13a71fd793606ec
Mehdi Tarhoult <mehdi.trh@outlook.fr>
2024-12-21 06:34:20 +0100

    Suppression du mot-clé "RUN" dans script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
-RUN apt-get update && apt-get install -y locales \
+apt-get update && apt-get install -y locales \
     && locale-gen fr_FR.UTF-8 \
     && update-locale LANG=fr_FR.UTF-8
