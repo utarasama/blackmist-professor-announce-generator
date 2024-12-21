32cf3e74cd69f2c216389290a5dc6c979540ebf9
Mehdi Tarhoult <mehdi.trh@outlook.fr>
2024-12-21 06:43:48 +0100

    Les jours ne sont plus détectés automatiquement Ils sont à présent un paramètre de la requête. C'est plus simple à gérer pour un serveur anglophone Suppression de script.sh par conséquent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from pandas import DataFrame
 from datetime import datetime
-import locale
 from fastapi import FastAPI
 from enum import Enum
 
@@ -12,19 +11,27 @@ class TrimestreEnum(int, Enum):
     trimestre3 = 3
 
 
+class JourEnum(str, Enum):
+    MONDAY = "LUNDI"
+    TUESDAY = "MARDI"
+    WEDNESDAY = "MERCREDI"
+    THURSDAY = "JEUDI"
+    #FRIDAY = "VENDREDI"
+    SATURDAY = "SAMEDI"
+    #SUNDAY = "DIMANCHE"
+
+
 app = FastAPI()
 
 
-def get_planning_from_dataframe(data: DataFrame, columns: tuple[str]) -> str:
+def get_planning_from_dataframe(data: DataFrame, columns: tuple[str], jour: JourEnum) -> str:
     # Récupération de la date du jour
     courses: str = ""
-    locale.setlocale(locale.LC_TIME, 'fr_FR.UTF-8')
-    today: str = datetime.now().strftime("%A").upper()  # "LUNDI", "MARDI", etc. en français
 
     index_col_to_check: int = None
     index_first_col: int = data.columns.get_loc(columns[0])
     for col_index in range(index_first_col + 1, index_first_col + 9):
-        if data.iloc[2, col_index] == today:
+        if data.iloc[2, col_index] == jour.value:
             index_col_to_check = col_index
             break
     reduced_data = data.iloc[:, [index_first_col, index_col_to_check]]
@@ -43,7 +50,7 @@ def get_planning_from_dataframe(data: DataFrame, columns: tuple[str]) -> str:
          summary="Générer l'annonce des cours",
          tags=["Black Mist RP"],
          response_description="Un texte en Markdown prêt à être envoyé sur Discord")
-def get_prof_announce(trimestre: TrimestreEnum):
+def get_prof_announce(trimestre: TrimestreEnum, jour: JourEnum):
     planning_urls: dict[str, str] = { 
         "1e année": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSj_SKPhUacbANqBmPyJp2f9Rq7J7vtqzx5vvVfKOkTl3IUbBjBrrucgiktiFu1pLoudDPG4PwqsR-j/pub?output=csv",
         "2e année": "https://docs.google.com/spreadsheets/d/e/2PACX-1vSj_SKPhUacbANqBmPyJp2f9Rq7J7vtqzx5vvVfKOkTl3IUbBjBrrucgiktiFu1pLoudDPG4PwqsR-j/pub?gid=2135775316&single=true&output=csv",
@@ -57,7 +64,7 @@ def get_prof_announce(trimestre: TrimestreEnum):
     complete_announce += '\n'
     for year, planning_url in planning_urls.items():
         data = pd.read_csv(planning_url)
-        planning: str = get_planning_from_dataframe(data, columns_per_trimestre[trimestre.value])
+        planning: str = get_planning_from_dataframe(data, columns_per_trimestre[trimestre.value], jour)
         complete_announce += f'## {year}'
         complete_announce += '\n'
         complete_announce += planning
--- script.sh
+++
@@ -1,4 +0,0 @@
-pip install -r requirements.txt
-apt-get update && apt-get install -y locales \
-    && locale-gen fr_FR.UTF-8 \
-    && update-locale LANG=fr_FR.UTF-8
